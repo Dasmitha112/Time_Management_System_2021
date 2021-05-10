e76fd017901cf7ca41dae105dd9348a35414847e
Dasmitha112 <54555529+Dasmitha112@users.noreply.github.com>
2021-05-10 19:09:08 +0530

    Revert "Add Sprint01 all codes"

---
+++ Time_Management_System_2021/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Time_Management_System_2021.Working_days_and_hours;
+
+namespace Time_Management_System_2021
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Generalpage());
+
+        }
+    }
+}
